07ebbee669efb7dc327bac1de77c2df909601f88
T0biasBerndt <82589193+T0biasBerndt@users.noreply.github.com>
2022-11-07 10:20:58 +0100

    Added Main Window

--- CryptoTracker/CryptoTracker/View/MainWindow.xaml.cs
+++ CryptoTracker/CryptoTracker/View/MainWindow.xaml.cs
@@ -10,13 +10,12 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CryptoTracker
+namespace CryptoTracker.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaktionslogik für MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
---
+++ CryptoTracker/CryptoTracker/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoTracker.ViewModel
+{
+    class MainWindowViewModel
+    {
+        private int _dashboardName;
+
+        public int DashboardName
+        {
+            get { return _dashboardName; }
+            set { _dashboardName = value; }
+        }
+
+    }
+}
